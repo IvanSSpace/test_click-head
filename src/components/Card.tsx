26107793a1b4abecd5f96e0ecc0377c88cd1906a
Ivan Sharapov <oldrum75@gmail.com>
2024-08-25 17:08:22 +0600

    fix: add button for open products[id]

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import Button from './ui/Button';
 import { useProductsStore } from '@/store/store';
+import { useRouter } from 'next/navigation';
 
 interface CardProps {
   title: string;
@@ -12,14 +13,19 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, price, id, inCart }) => {
   const toggleCart = useProductsStore((state) => state.toggleCart);
+  const router = useRouter();
 
   const handleClick = () => {
     toggleCart(id);
   };
 
+  const handleDivClick = () => {
+    router.push(`/products/${id}`);
+  };
+
   return (
     <div className="flex flex-col justify-between max-w-[320px] bg-white p-4 shadow-lg rounded-lg">
-      <div className='pb-3'>
+      <div className='pb-3' onClick={handleDivClick}>
         <div className="h-[216px] w-full bg-blue-red-gradient rounded-lg"></div>
         <h2 className="font-bold lg:text-xl text-market-red mt-2 text-lg">{price}</h2>
         <h2 className="text-market-blue mt-1 text-lg">{title}</h2>
